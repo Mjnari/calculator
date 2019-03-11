cab71dd2556d8992ceb1b9a52184f03e3ae3e55e
Mjnari <rich.mj1@gmail.com>
2019-03-10 23:10:57 -0500

    numbers show on display when clicked

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
-const add = function (x ,y){
+const add = function (x, y) {
     return x + y;
 }
 
-function subtract (x, y){
+function subtract(x, y) {
     return x - y;
 }
 
-function multiply (x, y){
+function multiply(x, y) {
     return x * y;
 }
 
-function divide (x, y){
+function divide(x, y) {
     return x / y;
 }
 
-function operate(operator, x, y){
-    if(operator == "+")
+function operate(operator, x, y) {
+    if (operator == "+")
         return add(x, y);
-    else if(operator == "-")
+    else if (operator == "-")
         return subtract(x, y);
-    else if(operator == "*")
+    else if (operator == "*")
         return multiply(x, y);
-    else if(operator == "/")
+    else if (operator == "/")
         return divide(x, y);
 }
+
+// set elements (is elements the right word?)
+const buttons = document.querySelectorAll("button");
+let display = document.querySelector("#display");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if(button.parentElement.parentElement.id == "numbers")
+            display.value += button.id;
+    })
+});
