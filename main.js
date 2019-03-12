93670d8c5c4cc83ccd45a04d31a9a4d4f68b2767
Mjnari <rich.mj1@gmail.com>
2019-03-11 23:08:44 -0500

    basic calculations done. Working on decimal

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// TODO: add "=" button to html and js
-
 const add = function (x, y) {
     return x + y;
 }
@@ -16,44 +14,93 @@ function divide(x, y) {
     return x / y;
 }
 
+/**
+ * 
+ * @param {string} operator "add", "subtract", "multiply", "divide"
+ * @param {float} x 
+ * @param {float} y 
+ */
 function operate(operator, x, y) {
-    if (operator == "+")
+    if (operator == "add")
         return add(x, y);
-    else if (operator == "-")
+    else if (operator == "subtract")
         return subtract(x, y);
-    else if (operator == "*")
+    else if (operator == "multiply")
         return multiply(x, y);
-    else if (operator == "/")
+    else if (operator == "divide")
         return divide(x, y);
+}
+
+/**
+ * Clears the display, stored value, and stored operator.
+ * Called when the clear button is pressed.
+ */
+function clear() {
+    display.value = "";
+    storedValue = 0;
+    storedOperator = "";
+}
+
+/**
+ * Clears the display without clearing stored value or stored operator.
+ * Useful for after an operation is run.
+ */
+function displayClear() {
+    display.value = "";
 }
 
 // set elements
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector("#display");
 
-let displayValue = "";
+// set global variables
+let storedValue = 0;
+let storedOperator = "";
+let displayClearFlag = 0;
 
 // buttons event listeners
 buttons.forEach((button) => {
+
     // numbers
-    if (button.parentElement.parentElement.id == "numbers")
+    if (button.getAttribute("class") == "number"){
         button.addEventListener("click", () => {
+            if (displayClearFlag == 1) {
+                displayClear();
+                displayClearFlag = 0;
+            }
             display.value += button.id;
-            displayValue += button.id;
         })
+    }
 
     // operators
-    if (button.parentElement.id == "operators")
+    if (button.getAttribute("class") == "operator"){
         button.addEventListener("click", () => {
-            // do nothing if user has not entered a number yet
-            if (displayValue != "") {
-                console.log(button.id)
-                switch(button.id) {
-                    case "add":
-                    case "subtract":
-                    case "multiply":
-                    case "divide":
-                }
+
+            if (display.value != "") { // do nothing if user has not entered a number yet
+                if (storedOperator != "") // if this is not a users first operation, then run the calculations before continuing
+                    display.value = operate(storedOperator, storedValue, Number.parseFloat(display.value));
+                storedValue = Number.parseFloat(display.value);
+                displayClearFlag = 1;
+                storedOperator = button.id;
             }
         })
+    }
+
+    // clear
+    if (button.id == "clear") {
+        button.addEventListener("click", () => {
+            clear();
+        });
+    }
+
+    // equal sign
+    if (button.id == "equal") {
+        button.addEventListener("click", () => {
+            if (storedOperator != "") { // do nothing if user hits equal sign without first picking an operator
+                display.value = operate(storedOperator, storedValue, Number.parseFloat(display.value));
+                storedValue = Number.parseFloat(display.value);
+                displayClearFlag = 1;
+            }
+        });
+    }
 });
